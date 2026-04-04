58a633e41096c80325beb3a056290586f7c685ce
Hamat09 <hamato0904alt@gmail.com>
2026-04-04 18:24:35 +0530

    Updated

--- .config/matugen/generated/vpn-status.sh
+++ .config/matugen/generated/vpn-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(warp-cli status)
 if [[ "$status" == *"Connected"* ]]; then
-  echo '{"text": "<span color=\"#897565\">VPN</span> | <span color=\"#95662d\">CONNECTED</span>", "class": "connected"}'
+  echo '{"text": "<span color=\"#0d9ba4\">VPN</span> | <span color=\"#049ca9\">CONNECTED</span>", "class": "connected"}'
 else
-  echo '{"text": "<span color=\"#897565\">VPN</span> | <span color=\"#978876\">DISCONNECTED</span>", "class": "disconnected"}'
+  echo '{"text": "<span color=\"#0d9ba4\">VPN</span> | <span color=\"#f6c43a\">DISCONNECTED</span>", "class": "disconnected"}'
 fi
---
+++ scripts/gowall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Choose what to do with image"
+OPT=$(printf "remove background\nconvert image to a theme\n")
