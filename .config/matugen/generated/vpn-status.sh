c28b1a8950b190ed5e1a1f2da611ff140e0c660c
Hamat09 <hamato0904alt@gmail.com>
2026-04-28 22:11:17 +0530

    Updated

--- .config/matugen/generated/vpn-status.sh
+++ .config/matugen/generated/vpn-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(warp-cli status)
 if [[ "$status" == *"Connected"* ]]; then
-  echo '{"text": "<span color=\"#31748f\">VPN</span> | <span color=\"#eb6f92\">CONNECTED</span>", "class": "connected"}'
+  echo '{"text": "<span color=\"#c4646b\">VPN</span> | <span color=\"#956566\">CONNECTED</span>", "class": "connected"}'
 else
-  echo '{"text": "<span color=\"#31748f\">VPN</span> | <span color=\"#c4a7e7\">DISCONNECTED</span>", "class": "disconnected"}'
+  echo '{"text": "<span color=\"#c4646b\">VPN</span> | <span color=\"#cd9b79\">DISCONNECTED</span>", "class": "disconnected"}'
 fi
--- scripts/power-profile.sh
+++ scripts/power-profile.sh
@@ -8,17 +8,16 @@ chosen="$(echo -e "$options" | rofi -dmenu -p "Power Profile" -i -l 3)"
 
 # Case statement to execute command based on selection
 case $chosen in
-    *Balanced)
-        powerprofilesctl set balanced
-        notify-send "Power Profile" "Balanced"
-        ;;
-    *Power-Saver)
-        powerprofilesctl set power-saver
-        notify-send "Power Profile" "Power-Saver"
-        ;;
-    *Performance)
-        powerprofilesctl set performance
-        notify-send "Power Profile" "Performance"
-        ;;
+*Balanced)
+  powerprofilesctl set balanced
+  notify-send "Power Profile" "Balanced"
+  ;;
+*Power-Saver)
+  powerprofilesctl set power-saver
+  notify-send "Power Profile" "Power-Saver"
+  ;;
+*Performance)
+  powerprofilesctl set performance
+  notify-send "Power Profile" "Performance"
+  ;;
 esac
-
