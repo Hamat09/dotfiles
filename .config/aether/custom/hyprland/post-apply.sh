55ce8af814e2c2f9f26dadab6b68e7e3a2edb6c8
Hamat09 <hamato0904alt@gmail.com>
2026-05-01 22:54:36 +0530

    Updated

--- .config/aether/custom/hyprland/post-apply.sh
+++ .config/aether/custom/hyprland/post-apply.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 hyprctl reload
+
+# 1. Define your wallpaper directory
+WALLPAPER_DIR="/home/hamato/.config/aether/theme/backgrounds"
+
+# 2. Pick a random image file from the directory
+# This finds files and picks one at random using 'shuf'
+SELECTED_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+# 3. Apply it using swww with a transition effect
+# Available types: simple, fade, left, right, top, bottom, wipe, wave, grow, center, any
+awww img "$SELECTED_WALLPAPER" --transition-type none
--- .config/aether/theme/vpn-status-vpn-status.sh
+++ .config/aether/theme/vpn-status-vpn-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(warp-cli status)
 if [[ "$status" == *"Connected"* ]]; then
-  echo '{"text": "<span color=\"#537e9c\">VPN</span> | <span color=\"#e1404a\">CONNECTED</span>", "class": "connected"}'
+  echo '{"text": "<span color=\"#FDAE62\">VPN</span> | <span color=\"#F2A097\">CONNECTED</span>", "class": "connected"}'
 else
-  echo '{"text": "<span color=\"#537e9c\">VPN</span> | <span color=\"#83799b\">DISCONNECTED</span>", "class": "disconnected"}'
+  echo '{"text": "<span color=\"#FDAE62\">VPN</span> | <span color=\"#AF87AC\">DISCONNECTED</span>", "class": "disconnected"}'
 fi
