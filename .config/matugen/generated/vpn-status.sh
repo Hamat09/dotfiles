310e7913762877229f367910f2a17f26e690cb0e
Hamat09 <hamato0904alt@gmail.com>
2026-07-18 00:27:42 +0530

    Updated

--- .config/matugen/generated/vpn-status.sh
+++ .config/matugen/generated/vpn-status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 status=$(warp-cli status)
 if [[ "$status" == *"Connected"* ]]; then
-  echo '{"text": "<span color=\"#63e1d1\">VPN</span> | <span color=\"#bc64b4\">CONNECTED</span>", "class": "connected"}'
+  echo '{"text": "<span color=\"#54b65a\">VPN</span> | <span color=\"#e2c296\">CONNECTED</span>", "class": "connected"}'
 else
-  echo '{"text": "<span color=\"#63e1d1\">VPN</span> | <span color=\"#95d0ab\">DISCONNECTED</span>", "class": "disconnected"}'
+  echo '{"text": "<span color=\"#54b65a\">VPN</span> | <span color=\"#85e0db\">DISCONNECTED</span>", "class": "disconnected"}'
 fi
